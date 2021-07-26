91fb5c82b878d9e6924707f9d6c83757db35e9bf
kbgro <daniendegwa@gmail.com>
2021-07-26 09:42:04 +0300

    custom field

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,8 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+from .fields import OrderField
+
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -35,9 +37,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=["course"])
 
     def __str__(self):
-        return self.title
+        return f"{self.order}. {self.title}"
+
+    class Meta:
+        ordering = ["order"]
 
 
 class Content(models.Model):
@@ -47,6 +53,10 @@ class Content(models.Model):
     )
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey("content_type", "object_id")
+    order = OrderField(blank=True, for_fields=["module"])
+
+    class Meta:
+        ordering = ["order"]
 
 
 class ItemBase(models.Model):
